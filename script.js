f7e6aeadeaefbe1d8960d5d84d11db59e15705f6
Lorteddy12 <lorteddy904@gmail.com>
2026-05-21 10:37:57 -0700

    wads
    yay

---
+++ script.js
@@ -0,0 +1,35 @@
+
+let animation = document.getElementById(ani);
+
+function goBack() {
+    window.history.back();
+}
+const projects = document.querySelectorAll('.proj');
+
+projects.forEach((proj) => {
+    // create overlay
+    const overlay = document.createElement('div');
+    overlay.classList.add('overlay');
+    proj.appendChild(overlay);
+
+    let text = proj.dataset.title;
+    let p = document.createElement("H2");
+    p.textContent = text;
+    overlay.appendChild(p);
+
+    // hover in
+    proj.addEventListener('mouseenter', () => {
+        overlay.style.opacity = '0';
+    });
+
+    // hover out
+    proj.addEventListener('mouseleave', () => {
+        overlay.style.opacity = '0.7';
+    });
+});
+// This just creates another page, and i already made html pages with numerical order, which was easy.
+projects.forEach((proj, index) => {
+    proj.addEventListener("click", () => {
+        window.location.href = `page${index + 1}.html`;
+    });
+});
