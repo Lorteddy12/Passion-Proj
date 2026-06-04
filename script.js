bf7fb11ab5e9b19564c167371da16978d48bd7c2
Lorteddy12 <lorteddy904@gmail.com>
2026-06-04 07:49:40 -0700

    se
    meep

--- script.js
+++ script.js
@@ -7,8 +7,6 @@ function goBack() {
 const projects = document.querySelectorAll('.proj');
 
 function switchPageAnimation() {
-    addEventListener
-}
 document.addEventListener('DOMContentLoaded', () => { // listener for when the page is loaded
     const links = document.querySelectorAll('a'); // grabs <a> tags and makes the a varible
     links.forEach(link => { //for each link that gets clicked it will send them to the page 
@@ -24,3 +22,37 @@ document.addEventListener('DOMContentLoaded', () => { // listener for when the p
       });
     });
   });
+}
+function buttonFadeOut(){
+  document.addEventListener('DOMContentLoaded',  () => {
+    const links = document.querySelectorAll('a');
+
+  })
+}
+  projects.forEach((proj) => {
+    // create overlay
+    const overlay = document.createElement('div');
+    overlay.classList.add('overlay');
+    proj.appendChild(overlay);
+
+    let text = proj.dataset.title;
+    let p = document.createElement("H2");
+    p.textContent = text;
+    overlay.appendChild(p);
+
+    // hover in
+    proj.addEventListener('mouseenter', () => {
+        overlay.style.opacity = '0';
+    });
+
+    // hover out
+    proj.addEventListener('mouseleave', () => {
+        overlay.style.opacity = '0.7';
+    });
+});
+// This just creates another page, and i already made html pages with numerical order, which was easy.
+projects.forEach((proj, index) => {
+    proj.addEventListener("click", () => {
+        window.location.href = `page${index + 1}.html`;
+    });
+});
